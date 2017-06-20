50b6a54c30c3d7317982b147d0b44855ed7d79e4
jaguuuar <sirkamson@gmail.com>
2017-06-20 10:49:45 +0200

    Square class in square module - DONE

---
+++ square.py
@@ -0,0 +1,20 @@
+class Square:
+
+    def __init__(self, row, column, is_ship_part, is_hit=False):
+        self.row = row
+        self.column = column
+        self.is_ship_part = is_ship_part
+        self.is_hit = is_hit
+
+
+    def hit(self):
+        self.is_hit = True
+
+    def __str__(self):
+        if self.is_hit:
+            if self.is_ship_part:
+                return 'X'
+            else:
+                return 'O'
+        else:
+            return ' '
