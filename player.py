42d2d3f21d2f1c0a7074923591fe9e06b3013cde
jaguuuar <sirkamson@gmail.com>
2017-06-21 12:44:42 +0200

    Add is_winner method

--- player.py
+++ player.py
@@ -8,14 +8,20 @@ class Player:
         self.name = name
         self.is_winner = False
         self.sunk_ships = 0
+        self.player_ships = []
 
-    def sunk_ships_count(self):
-        if ship.check_if_sunk() == True:
+
+    def sunk_ships_count(self, ship):
+        if ship.is_sunk == True:
             self.sunk_ships += 1
 
     def check_is_winner(self):
-        if self.sunk_ships == 1:
-            is_winner = True
+        if self.sunk_ships == 2:
+            self.is_winner = True
+
+    def add_ship(self, ship):
+        self.player_ships.append(ship)
+
 
     def __str__(self):
         if self.is_winner:
