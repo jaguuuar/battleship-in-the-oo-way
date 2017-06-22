3f63bfcf04ef06fb3ac8631dce94f60a0ae5973a
domakdomak <domagalski.mt@gmail.com>
2017-06-22 09:27:31 +0200

    Add docstrings in Square

--- square.py
+++ square.py
@@ -4,11 +4,18 @@ class Square:
         self.ship = ship
         self.is_hit = is_hit
 
-
     def hit(self):
+        '''
+        Marks attribute is_hit of Square object as True
+        '''
         self.is_hit = True
 
     def __str__(self):
+        # TODO:
+        '''
+        Returns 'X' if is_hit attribute of Square object is True,
+        otherwise returns ' ' (empty string)
+        '''
         if self.is_hit:
             if self.ship is not None:
                 return 'Z'
