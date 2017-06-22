0923525846b540f2172a3db38e19d31f4d856b20
jaguuuar <sirkamson@gmail.com>
2017-06-22 16:23:41 +0200

    update readme.md

--- player.py
+++ player.py
@@ -89,18 +89,16 @@ class Player:
         for ship in self.ships:
             if ship.is_sunk:
                 self.enemy_sunk_ships += 1
+                
 
     def check_is_winner(self):
         if self.enemy_sunk_ships == 2:
             self.is_winner = True
 
+
     def add_ship(self, ship):
         self.ships.append(ship)
 
-    def remove_ship(self):
-        for ship in self.ships:
-            if ship.is_sunk:
-                self.ships.remove(ship)
 
     def __str__(self):
         if self.is_winner:
