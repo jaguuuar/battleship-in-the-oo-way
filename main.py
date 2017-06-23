3ad1074b417de5230d2a8536fd54e5df56b2757f
domakdomak <domagalski.mt@gmail.com>
2017-06-23 09:36:25 +0200

    You can now Exit from game

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from ocean import Ocean
 import os
 from battlefield import BattleField
 
+
 def main():
     battlefield = BattleField()
     print('\nWelcome to the Battleship game!')
-    choice = 0
-    possible_choices = [1, 2, 3]
+    choice = ''
+    possible_choices = [1, 2, 3, 0]
     while choice not in possible_choices:
-        print('1) for 2 Players\n2) for 1 Player\n3) for PC vs PC')
+        print('1) for 2 Players\n2) for 1 Player\n3) for PC vs PC\n0) Exit')
         try:
             choice = int(input("Choose game mode: "))
         except ValueError:
@@ -20,6 +21,8 @@ def main():
         battlefield.start_pvc_game()
     elif choice == 3:
         battlefield.start_cvc_game()
+    elif choice == 0:
+        exit()
 
 if __name__ == '__main__':
     main()
